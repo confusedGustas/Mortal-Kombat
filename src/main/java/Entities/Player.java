43b7cf9f62411f5f72cf4c81aa4a15422bf3a987
Gustas Antanas Gaigalas <gustas@Gustass-MacBook-Air.local>
2023-10-23 03:27:30 +0300

    fixed shared instances

--- src/main/java/Entities/Player.java
+++ src/main/java/Entities/Player.java
@@ -2,14 +2,14 @@ package Entities;
 
 public class Player {
     public final String name;
-    public static int HP, M, S, Sw;
+    private int HP, M, S, Sw;
 
-    public Player(String PlayerName) {
-        this.name = PlayerName;
-        HP = 3000;
-        M = 35;
-        S = 25;
-        Sw = 20;
+    public Player(String playerName) {
+        this.name = playerName;
+        this.HP = 3000;
+        this.M = 35;
+        this.S = 25;
+        this.Sw = 20;
     }
 
     public String getName() {
